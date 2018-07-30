2e199469cf0356d2ac807c5d2d6e053691f8b018
ygy <40139861+vviat@users.noreply.github.com>
2018-07-30 22:03:39 +0800

    二分查找常规做法

---
+++ chazhao/erfenchazhao.c
@@ -0,0 +1,32 @@
+/*************************************************************************
+	> File Name: erfenchazhao.c
+	> Author: 
+	> Mail: 
+	> Created Time: 2018年07月28日 星期六 15时23分57秒
+ ************************************************************************/
+
+#include<stdio.h>
+//正常问题
+int binary_search1(int *num, int n, int x){
+    int head = 0; 
+    int tail = n - 1;
+    int mid;
+    while(head <= tail) {
+        mid = (head + tail) >> 1;
+        if(num[mid] == x) return mid;
+        if(num[mid] < x) {
+            head = mid + 1;
+        } else {
+            tail = mid - 1;
+        }
+    } 
+    return -1;
+}
+
+int main(){
+    int n;
+    int num[n];
+
+
+    return 0;
+}
