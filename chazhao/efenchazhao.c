0c10038e504e2b0e74ff7e0285d1ac4c3025cf0a
ygy <40139861+vviat@users.noreply.github.com>
2018-07-30 22:00:22 +0800

    一堆1一堆0找最后的1

--- chazhao/efenchazhao.c
+++ chazhao/efenchazhao.c
@@ -1,19 +1,18 @@
 #include<stdio.h>
-//正常问题
-int binary_search1(int *num, int n, int x){
-    int head = 0; 
+//111111000000
+int binary_search2(int *num, int n, int x){
+    int head = -1; 
     int tail = n - 1;
     int mid;
-    while(head <= tail) {
-        mid = (head + tail) >> 1;
-        if(num[mid] == x) return mid;
-        if(num[mid] < x) {
-            head = mid + 1;
+    while(head < tail) {
+        mid = (head + tail + 1) >> 1;
+        if(num[mid] == 1) {
+            head = mid;
         } else {
             tail = mid - 1;
         }
     } 
-    return -1;
+    return head;
 }
 int main(){
     int n;
